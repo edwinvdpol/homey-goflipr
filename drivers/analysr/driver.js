3faabf0331fd63577356dcf1f9ef2f0518e4bbda
Edwin van de Pol <developer@edwinvandepol.nl>
2022-07-17 00:05:06 +0200

    Rewrite

--- drivers/analysr/driver.js
+++ drivers/analysr/driver.js
@@ -1,8 +1,48 @@
 'use strict';
 
 const Driver = require('../../lib/Driver');
+const {blank} = require('../../lib/Utils');
 
 class AnalysrDriver extends Driver {
+
+  // Pair devices
+  async onPairListDevices({oAuth2Client}) {
+    this.log('Listing devices');
+
+    const modules = await oAuth2Client.getModules();
+
+    let devices = [];
+
+    // No devices found
+    if (blank(modules.analysr)) {
+      return devices;
+    }
+
+    for (const module of modules.analysr) {
+      const pool = await oAuth2Client.getPool(module.Serial);
+
+      devices.push(this.getDeviceData(module, pool));
+    }
+
+    return devices;
+  }
+
+  // Get data to create the device
+  getDeviceData(module, pool) {
+    return {
+      name: `${pool.Type.Name} (${module.Serial})`,
+      data: {
+        id: module.Serial
+      },
+      settings: {
+        serial_number: module.Serial,
+        version: String(module.Version),
+        volume: String(pool.Volume) + ' m³',
+        construction_year: String(pool.BuiltYear)
+      }
+    }
+  }
+
 }
 
 module.exports = AnalysrDriver;
