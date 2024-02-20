bf01c0c1955d65020e3f6fbdf47bc78161e4161b
Edwin van de Pol <developer@edwinvandepol.nl>
2024-02-20 19:57:14 +0100

    Replaced `filled` with `in` check

--- drivers/analysr/device.js
+++ drivers/analysr/device.js
@@ -16,35 +16,35 @@ class AnalysrDevice extends Device {
     this.log('[Sync]', JSON.stringify(data));
 
     // Temperature
-    if (filled(data.Temperature)) {
+    if ('Temperature' in data) {
       this.setCapabilityValue('measure_temperature', data.Temperature).catch(this.error);
     }
 
     // Desinfectant
-    if (filled(data.Desinfectant)) {
-      if (filled(data.Desinfectant.Value)) {
+    if ('Desinfectant' in data) {
+      if ('Value' in data.Desinfectant) {
         this.setCapabilityValue('measure_ci', data.Desinfectant.Value).catch(this.error);
       }
 
-      if (filled(data.Desinfectant.DeviationSector)) {
+      if ('DeviationSector' in data.Desinfectant) {
         this.setCapabilityValue('status_ci', data.Desinfectant.DeviationSector).catch(this.error);
       }
     }
 
     // PH value
-    if (filled(data.PH)) {
-      if (filled(data.PH.Value)) {
+    if ('PH' in data) {
+      if ('Value' in data.PH) {
         this.setCapabilityValue('measure_ph', data.PH.Value).catch(this.error);
       }
 
-      if (filled(data.PH.DeviationSector)) {
+      if ('DeviationSector' in data.PH) {
         this.setCapabilityValue('status_ph', data.PH.DeviationSector).catch(this.error);
       }
     }
 
     // Oxydo reduction potentiel
-    if (filled(data.OxydoReductionPotentiel)) {
-      if (filled(data.OxydoReductionPotentiel.Value)) {
+    if ('OxydoReductionPotentiel' in data) {
+      if ('Value' in data.OxydoReductionPotentiel) {
         this.setCapabilityValue('measure_orp', data.OxydoReductionPotentiel.Value).catch(this.error);
       }
     }
