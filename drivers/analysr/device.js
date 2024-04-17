ca0dc620c2b5081d9d9e604c4034412d70eafa6d
Edwin van de Pol <developer@edwinvandepol.nl>
2024-04-17 15:16:54 +0200

    Merge device and driver

--- drivers/analysr/device.js
+++ drivers/analysr/device.js
@@ -1,55 +1,8 @@
 'use strict';
 
 const Device = require('../../lib/Device');
-const { blank } = require('../../lib/Utils');
 
 class AnalysrDevice extends Device {
-
-  /*
-  | Synchronization functions
-  */
-
-  // Handle sync data
-  async handleSyncData(data) {
-    if (blank(data)) return;
-
-    this.log('[Sync]', JSON.stringify(data));
-
-    // Temperature
-    if ('Temperature' in data) {
-      this.setCapabilityValue('measure_temperature', data.Temperature).catch(this.error);
-    }
-
-    // Desinfectant
-    if ('Desinfectant' in data) {
-      if ('Value' in data.Desinfectant) {
-        this.setCapabilityValue('measure_ci', data.Desinfectant.Value).catch(this.error);
-      }
-
-      if ('DeviationSector' in data.Desinfectant) {
-        this.setCapabilityValue('status_ci', data.Desinfectant.DeviationSector).catch(this.error);
-      }
-    }
-
-    // PH value
-    if ('PH' in data) {
-      if ('Value' in data.PH) {
-        this.setCapabilityValue('measure_ph', data.PH.Value).catch(this.error);
-      }
-
-      if ('DeviationSector' in data.PH) {
-        this.setCapabilityValue('status_ph', data.PH.DeviationSector).catch(this.error);
-      }
-    }
-
-    // Oxydo reduction potentiel
-    if ('OxydoReductionPotentiel' in data) {
-      if ('Value' in data.OxydoReductionPotentiel) {
-        this.setCapabilityValue('measure_orp', data.OxydoReductionPotentiel.Value).catch(this.error);
-      }
-    }
-  }
-
 }
 
 module.exports = AnalysrDevice;
--- drivers/analysr/driver.js
+++ drivers/analysr/driver.js
@@ -3,33 +3,6 @@
 const Driver = require('../../lib/Driver');
 
 class AnalysrDriver extends Driver {
-
-  // Return data to create the device
-  async getDeviceData(client, device) {
-    this.log(`Get device ${device.Serial} from API`);
-
-    let pool = await client.getDevice(device.Serial);
-
-    const data = {
-      name: `${pool.Type.Name} (${device.Serial})`,
-      data: {
-        id: device.Serial,
-      },
-      settings: {
-        serial_number: device.Serial,
-        version: `${device.Version}`,
-        volume: `${pool.Volume} m³`,
-        construction_year: `${pool.BuiltYear}`,
-      },
-    };
-
-    pool = null;
-
-    this.log('Device found', JSON.stringify(data));
-
-    return data;
-  }
-
 }
 
 module.exports = AnalysrDriver;
